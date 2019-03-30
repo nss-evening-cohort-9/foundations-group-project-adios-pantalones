7cb0e0ec189ab20cef2f32479675d779a227144b
Dev Davis <sean.dev.davis@gmail.com>
2019-03-30 12:35:35 -0500

    wip

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log("Suh dude");
+const wrestlers =[
+    {
+        name: 'Stone Cold Steve Austin',
+        imgUrl: './assets/Stone-Cold-Steve-Austin.jpg',
+        about: 'Stone Cold Steve Austin makes beer for those who are out there crushing it. It hits heavy and goes down smooth. You may just wanna drink two at a time.',
+        beerOfChoice: 'Steveweisers'
+    },
+    {
+        name: 'Andre The Giant',
+        imgUrl: './assets/Andre.jpg',
+        about: 'Stone Cold Steve Austin makes beer for those who are out there crushing it. It hits heavy and goes down smooth. You may just wanna drink two at a time.',
+        beerOfChoice: ''
+    },
+    {
+        name: 'Macho Man Randy Savage',
+        imgUrl: './assets/Macho man.jpg',
+        about: 'Stone Cold Steve Austin makes beer for those who are out there crushing it. It hits heavy and goes down smooth. You may just wanna drink two at a time.',
+        beerOfChoice: ''
+    },
+    {
+        name: 'Sgt Slaughter',
+        imgUrl: './assets/Sgt Slghtr.jpg',
+        about: 'Stone Cold Steve Austin makes beer for those who are out there crushing it. It hits heavy and goes down smooth. You may just wanna drink two at a time.',
+        beerOfChoice: ''
+    },
+    {
+        name: 'Sting',
+        imgUrl: './assets/Sting.jpg',
+        about: "When he's not smashing folks with his baseball bat, Sting is brewing up some of the most delicious IPA's you've ever put on your tastebuds.",
+        beerOfChoice: ''
+    },
+    {
+        name: 'Ric Flair',
+        imgUrl: './assets/nature boy.jpg',
+        about: 'Stone Cold Steve Austin makes beer for those who are out there crushing it. It hits heavy and goes down smooth. You may just wanna drink two at a time.',
+        beerOfChoice: ''
+    },
+    {
+        name: 'Ultimate Warrior',
+        imgUrl: './assets/UW.jpg',
+        about: 'Stone Cold Steve Austin makes beer for those who are out there crushing it. It hits heavy and goes down smooth. You may just wanna drink two at a time.',
+        beerOfChoice: ''
+    }
+]
