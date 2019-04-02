ed8ebf3f25229d4c9d9f956ff527c6561b8897a3
Greg Stephen <GregAStephen@gmail.com>
2019-04-01 19:15:44 -0500

    WIP

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const brews = [
         imgURL: './assets/Beer1.jpg',
         type: 'Ale',
         abv: '9.4',
+        status: 'heavy',
         ibu: '88',
         tastingNotes: ['Citrusy', 'Hoppy', 'Crisp'],
     },{
@@ -15,6 +16,7 @@ const brews = [
         imgURL: './assets/Beer2.jpg',
         type: 'Ale',
         abv: '6.6',
+        status: 'cruiser',
         ibu: '25',
         tastingNotes: ['Hazy', 'Leathery'],
     },{
@@ -22,6 +24,7 @@ const brews = [
         imgURL: './assets/Beer3.jpg',
         type: 'Lager',
         abv: '2',
+        status: 'light',
         ibu: '5',
         tastingNotes: ['Light', 'Metallic', ],
     },{
@@ -29,6 +32,7 @@ const brews = [
         imgURL: './assets/Beer4.jpg',
         type: 'Ale',
         abv: '12.9',
+        status: 'heavy',
         ibu: '50',
         tastingNotes: ['Boozy', 'Bready', 'Robust'],
     },{
@@ -36,6 +40,7 @@ const brews = [
         imgURL: '/assets/Beer5.jpg',
         type: 'Lager',
         abv: '4.5',
+        status: 'cruiser',
         ibu: '35',
         tastingNotes: ['Crisp', 'Light', 'Buttery'],
     },{
@@ -43,6 +48,7 @@ const brews = [
         imgURL: './assets/Beer6.jpg',
         type: 'Ale',
         abv: '5.9',
+        status: 'cruiser',
         ibu: '80',
         tastingNotes: ['Heavy', 'Malty', 'Caramely'],
     },
@@ -50,11 +56,14 @@ const brews = [
 const filterFormRevealBtn = document.getElementById("filterFormRevealBtn");
 const filterBeerBtn = document.getElementById("filterBtn");
 const filterForm = document.getElementById("filterForm");
+const filterRadios = document.getElementsByName("abv");
 
-const brewCardBuilder = (arrayOfBrews, checkboxValue) => {
+
+const brewCardBuilder = (arrayOfBrews, checkboxValue, abvValue) => {
+console.log(abvValue);
     let domString = '';
     arrayOfBrews.forEach((beer) => {
-        if(checkboxValue.length === 0){
+        const brewCardCreater = () => {
             domString += `<div class="beerCard col-4">`;
             domString +=    `<h1 class="beerName">${beer.name}</h1>`;
             domString +=    `<div class="beerDescription">`;
@@ -69,36 +78,43 @@ const brewCardBuilder = (arrayOfBrews, checkboxValue) => {
             domString +=        `</p>`;
             domString +=    `</div>`;
             domString += `</div>`; 
-        } else if (checkboxValue.includes(beer.type)){
-            domString += `<div class="beerCard col-4">`;
-            domString +=    `<h1 class="beerName">${beer.name}</h1>`;
-            domString +=    `<div class="beerDescription">`;
-            domString +=        `<img class="beerImage" src=${beer.imgURL}>`;
-            domString +=        `<p class="beerType">Type: ${beer.type}</p>`;
-            domString +=        `<p class="beerABV">ABV: ${beer.abv}%</p>`;
-            domString +=        `<p class="beerIBU">IBU: ${beer.ibu}</p>`;
-            domString +=        `<p> Tasting Notes: `;
-            beer.tastingNotes.forEach((notes) => {
-            domString +=        `${notes} `;
-            })
-            domString +=        `</p>`;
-            domString +=    `</div>`;
-            domString += `</div>`; 
+            return domString;
+        }
+
+        if (abvValue === beer.status) {
+            domString = brewCardCreater();
+            } else if (checkboxValue.includes(beer.type)){
+            domString = brewCardCreater();
         }
     });
     printToDom("brewDiv", domString);
 };
 
-function checkboxFilter(){
+
+const checkboxFilter = () => {
     let selectedItems= [];
     let itemsType=document.getElementsByName('beer');
     for(let i=0; i<itemsType.length; i++){
         if(itemsType[i].type=='checkbox' && itemsType[i].checked==true)
             selectedItems.push(itemsType[i].value);
     }
-    brewCardBuilder(brews, selectedItems);
+    return selectedItems;
+};
+
+
+const abvFilter = () => {
+    for (let i =0; i < filterRadios.length; i++) {
+        if (filterRadios[i].checked) {
+           return filterRadios[i].value;
+        }
+    }
 }
 
+const filterBeerFuntion = () => {
+    let filteredBeerType = checkboxFilter();
+    let filteredBeerAbv = abvFilter();
+    brewCardBuilder(brews, filteredBeerType, filteredBeerAbv);
+}
 const revealForm = () => {
     filterForm.style.display = 'block';
     filterFormRevealBtn.style.display = 'none';
@@ -110,15 +126,24 @@ const printToDom = (divId, textToPrint) => {
     selectedDiv.innerHTML = textToPrint;
 };
 
-const eventListeners = () => {
+const brewsEventListeners = () => {
     filterFormRevealBtn.addEventListener('click', revealForm);
-    filterBeerBtn.addEventListener('click', checkboxFilter);
+    filterBeerBtn.addEventListener('click', filterBeerFuntion);
 };
 
 
 const init = () => {
-    brewCardBuilder(brews, []);
-    eventListeners();
+    if(window.location.pathname === `/brews.html`) {
+        brewCardBuilder(brews, []);
+        brewsEventListeners();
+    } else if (window.location.pathname === `/brewmaster.html`) {
+        brewMasterCards(wrestlers);
+    } else if (window.location.pathname === `/index.html`) {
+
+    } else if (window.location.pathname === `/aboutus.html`) {
+
+    }
+   
 }
 
 init();
